18220ce8be559e4b188a6372d34b38599c60bd4e
motty93 <rdwbocungelt5@gmail.com>
2019-12-10 16:10:47 +0900

    update: sed成功時のみ実行

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sed -e "s/username/$USER/g" ./init_entrypoint.sh > ./entrypoint.sh
-docker-compose run web rails new . --database=postgresql -B -M --skip-yarn --skip-test --skip-turbolinks
-docker-compose build
-docker-compose run web bundle install ---path=vendor/bundle
-docker-compose run web rails db:create
-docker-compose run web rails db:migrate
-docker-compose up -d
+sed -e "s/username/$USER/g" ./init/init_entrypoint.sh > ./init/entrypoint.sh
+if [ $? = 0 ]; then
+  docker-compose run web rails new . --database=postgresql -B -M --skip-yarn --skip-test --skip-turbolinks
+  docker-compose build
+  docker-compose run web bundle install ---path=vendor/bundle
+  docker-compose run web rails db:create
+  docker-compose run web rails db:migrate
+  docker-compose up -d
+fi
