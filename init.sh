ef5050bcc2a2f370e17813b69db066bd10bdfb57
motty93 <rdwbocungelt5@gmail.com>
2019-12-10 18:14:11 +0900

    update: initスクリプト更新

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sed -e "s/username/$USER/g" ./init/init_entrypoint.sh > ./init/entrypoint.sh
-if [ $? = 0 ]; then
-  docker-compose run web rails new . --database=postgresql -B -M --skip-yarn --skip-test --skip-turbolinks
-  docker-compose build
-  docker-compose run web bundle install ---path=vendor/bundle
-  docker-compose run web rails db:create
-  docker-compose run web rails db:migrate
-  docker-compose up -d
-fi
+docker-compose run web rails new . --force --database=postgresql -B --skip-yarn --skip-test --skip-turbolinks
+
+echo "Linux用権限付与"
+sudo chown -R $USER:$USER *
+
+echo "force files checkout"
+git checkout Gemfile README.md .gitignore
+
+echo "application build"
+docker-compose build
+docker-compose run web bundle install --path=vendor/bundle
+docker-compose run web rails db:create
+docker-compose run web rails db:migrate
+docker-compose up -d
