d8e3fd1819119cbbf2c87c5a1537717ad6661cc6
motty93 <rdwbocungelt5@gmail.com>
2019-12-12 16:33:31 +0900

    update: コメント追加

--- init.sh
+++ init.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-docker-compose run web rails _5.2_ new . -s --database=postgresql \
+echo "rails new"
+docker-compose run web rails _5.2.4_ new . --skip --database=postgresql \
   --skip-git --skip-bundle --skip-turbolinks --skip-coffee --skip-test
 
 if [ "$(uname)" == 'Linux' ]; then
-  echo "Linux用権限付与"
+  echo "Linux chown all file"
   sudo chown -R $USER:$USER *
 fi
 
 echo "force files checkout"
 git checkout README.md .gitignore
 
-echo "application build"
+echo "fig build"
 docker-compose build
 
 if [ "$(uname)" == 'Linux' ]; then
-  echo "Linux用権限付与"
+  echo "Linux chown all file"
   sudo chown -R $USER:$USER *
 fi
 
-echo "bundle install & db setting"
+echo "bundle install"
 docker-compose run web bundle install --path=vendor/bundle
+
+echo "copy env setting"
+cp ./init/env_sample ./.env
+
+echo "copy database.yml"
+\cp -f ./init/init_database.yml ./config/database.yml
+
+echo "db create & migrate"
 docker-compose run web rails db:create
 docker-compose run web rails db:migrate
+
+echo "fig up"
 docker-compose up -d
