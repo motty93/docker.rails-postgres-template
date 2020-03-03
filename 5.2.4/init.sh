2cf68c0271b143e14f654c28a38e35f54b133073
motty93 <rdwbocungelt5@gmail.com>
2020-03-03 22:22:09 +0900

    update: skip spring & remove container

--- 5.2.4/init.sh
+++ 5.2.4/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "rails new"
-docker-compose run web rails _5.2.4_ new . --skip --database=postgresql \
-  --skip-git --skip-turbolinks --skip-coffee --skip-test
+docker-compose run --rm web rails _5.2.4_ new . --skip --database=postgresql \
+  --skip-git --skip-turbolinks --skip-coffee --skip-test --skip-spring
 
 if [ "$(uname)" == 'Linux' ]; then
   echo "Linux chown all file"
@@ -21,7 +21,7 @@ if [ "$(uname)" == 'Linux' ]; then
 fi
 
 echo "bundle install"
-docker-compose run web bundle install --path=vendor/bundle
+docker-compose run --rm web bundle install --path=vendor/bundle
 
 echo "copy env setting"
 cp ./docker/init/env_sample ./.env
@@ -30,8 +30,8 @@ echo "copy database.yml"
 \cp -f ./docker/init/init_database.yml ./config/database.yml
 
 echo "db create & migrate"
-docker-compose run web rails db:create
-docker-compose run web rails db:migrate
+docker-compose run --rm web rails db:create
+docker-compose run --rm web rails db:migrate
 
 echo "fig up deamon"
 docker-compose up -d
